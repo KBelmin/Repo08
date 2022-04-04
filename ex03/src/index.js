616a62143a84f61ed0bee3757d8e899eeeb2b524
KBelmin <ariardorn@icloud.com>
2022-04-04 14:42:06 +0200

    Used a local function

---
+++ ex03/src/index.js
@@ -0,0 +1,12 @@
+function localScope() {
+    var myVariable = "I am Here!";
+    console.log(myVariable);
+}
+localScope();
+
+if (typeof myVariable != "undefined") {
+    console.log('outside localScope', myVariable)
+} else {
+    console.log('outside localScope UNDEFINED!!!')
+}
+module.exports = localScope;
