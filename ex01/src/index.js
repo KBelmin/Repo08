197ec32b190976cccd44cfd55f9fc0181ac70cfe
KBelmin <ariardorn@icloud.com>
2022-04-04 14:05:56 +0200

    Used a function

---
+++ ex01/src/index.js
@@ -0,0 +1,8 @@
+function reusableFunction() {
+    // Write code below this line
+        console.log("Hi Padawan!");
+    // Write code above this line
+}
+
+reusableFunction();
+module.exports = reusableFunction;
